f3ab9b0c32eac275d4d0a8440d30c463d487e9da
swittum <122154159+swittum@users.noreply.github.com>
2023-12-05 01:08:06 +0100

    Added images

--- test.py
+++
@@ -1,8 +0,0 @@
-from dateutil import parser
-
-def format_time(timestamp):
-    parsed_timestamp = parser.parse(timestamp)
-    hours = parsed_timestamp.hour
-    minutes = parsed_timestamp.minute
-    formatted_time = f"{hours:02}:{minutes:02}"
-    return formatted_time
